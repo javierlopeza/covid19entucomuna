e9e10dcf57156aa27680ea6e7ea8c7bc4c58b50e
Javier López <javierlopez@uc.cl>
2020-04-25 19:09:20 -0400

    feat(place): center text inside container

--- src/components/PlaceLink.js
+++ src/components/PlaceLink.js
@@ -2,10 +2,13 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const PlaceLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
   margin: 5px;
   padding: 5px 10px;
   border-radius: 5px;
-  text-align: center;
   background-color: white;
 `;
 
