a99f34cc44be548994fd07391f3e0bd53b59f566
Javier López <javierlopez@uc.cl>
2020-04-25 02:01:18 -0400

    feat(font): add gotham

--- src/styles/global.js
+++ src/styles/global.js
@@ -4,6 +4,10 @@ import theme from './theme';
 export default createGlobalStyle`
   body {
     background-color: ${theme.colors.blue.light};
+  }
+  * {
+    font-family: 'GothamPro';
+    font-weight: 100;
     font-size: 18px;
   }
 `;
