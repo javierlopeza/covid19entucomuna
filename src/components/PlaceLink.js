6892b88a5da3332487a4b3fe09263cf3fca563f8
Javier López <javierlopez@uc.cl>
2020-04-26 19:35:59 -0400

    fix(places): remove hover animation in mobile

--- src/components/PlaceLink.js
+++ src/components/PlaceLink.js
@@ -11,19 +11,20 @@ const PlaceLink = styled(Link)`
   border-radius: 5px;
   background-color: white;
   ${({ theme }) => theme.baseShadow}
-  
+
   color: #6b6b6b;
   font-weight: 200;
 
-  background: linear-gradient(to left, white 50%, #5b78ff 50%);
-  background-size: 201% 100%;
-  background-position: right bottom;
-  
   transition: all 0.3s ease-in-out 0.2s;
 
-  &:hover {
-    color: white;
-    background-position: left bottom;
+  @media (hover: hover) {
+    background: linear-gradient(to left, white 50%, #5b78ff 50%);
+    background-size: 201% 100%;
+    background-position: right bottom;
+    :hover {
+      color: white;
+      background-position: left bottom;
+    }
   }
 `;
 
