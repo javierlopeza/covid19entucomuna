c6421d0dc2f94b29eebfc6eea318cdfa4c157d0c
Javier López <javierlopez@uc.cl>
2020-04-26 17:49:26 -0400

    feat(style): add sliding background to places links

--- src/components/PlaceLink.js
+++ src/components/PlaceLink.js
@@ -14,14 +14,16 @@ const PlaceLink = styled(Link)`
   
   color: #6b6b6b;
   font-weight: 200;
+
+  background: linear-gradient(to left, white 50%, #5b78ff 50%);
+  background-size: 201% 100%;
+  background-position: right bottom;
   
-  transition: all 0.15s ease-in-out;
+  transition: all 0.3s ease-in-out 0.2s;
 
   &:hover {
-    -webkit-box-shadow: 0px 2px 5px rgba(0,0,0,0.3);
-    -moz-box-shadow: 0px 2px 5px rgba(0,0,0,0.3);
-    box-shadow: 0px 2px 5px rgba(0,0,0,0.3);
-    color: #25343f;
+    color: white;
+    background-position: left bottom;
   }
 `;
 
