7bb1e4aba83b2b0346fe55b09de303d4f2b56f0a
Javier López <javierlopez@uc.cl>
2020-04-30 04:47:51 -0400

    feat(backup): add csv files urls

--- src/environment.js
+++ src/environment.js
@@ -3,4 +3,8 @@ export default {
   totalesNacionalesCsvUrl: process.env.REACT_APP_TOTALES_NACIONALES_CSV_URL,
   fallecidosPorRegionCsvUrl: process.env.REACT_APP_FALLECIDOS_POR_REGION_CSV_URL,
   confirmadosPorRegionCsvUrl: process.env.REACT_APP_CONFIRMADOS_POR_REGION_CSV_URL,
+
+  backupTotalesNacionalesCsvUrl: process.env.REACT_APP_BACKUP_TOTALES_NACIONALES_CSV_URL,
+  backupFallecidosPorRegionCsvUrl: process.env.REACT_APP_BACKUP_FALLECIDOS_POR_REGION_CSV_URL,
+  backupConfirmadosPorRegionCsvUrl: process.env.REACT_APP_BACKUP_CONFIRMADOS_POR_REGION_CSV_URL,
 };
