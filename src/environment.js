16289e066c6d4b8b982aacea3703bbb994728b33
Javier López <javierlopez@uc.cl>
2020-05-02 12:01:48 -0400

    feat(backup): add ActivosPorComuna to env

--- src/environment.js
+++ src/environment.js
@@ -4,6 +4,7 @@ export default {
   fallecidosPorRegionCsvUrl: process.env.REACT_APP_FALLECIDOS_POR_REGION_CSV_URL,
   confirmadosPorRegionCsvUrl: process.env.REACT_APP_CONFIRMADOS_POR_REGION_CSV_URL,
 
+  backupActivosPorComunaCsvUrl: process.env.REACT_APP_BACKUP_ACTIVOS_POR_COMUNA_CSV_URL,
   backupTotalesNacionalesCsvUrl: process.env.REACT_APP_BACKUP_TOTALES_NACIONALES_CSV_URL,
   backupFallecidosPorRegionCsvUrl: process.env.REACT_APP_BACKUP_FALLECIDOS_POR_REGION_CSV_URL,
   backupConfirmadosPorRegionCsvUrl: process.env.REACT_APP_BACKUP_CONFIRMADOS_POR_REGION_CSV_URL,
