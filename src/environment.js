2d2890b172bf786e70d3552c8c037cef35a754c6
Javier López <javierlopez@uc.cl>
2020-05-03 23:22:34 -0400

    del: remove minciencia-fetcher

--- src/environment.js
+++ src/environment.js
@@ -1,13 +1,3 @@
 export default {
-  activosPorComunaCsvUrl: process.env.REACT_APP_ACTIVOS_POR_COMUNA_CSV_URL,
-  totalesNacionalesCsvUrl: process.env.REACT_APP_TOTALES_NACIONALES_CSV_URL,
-  fallecidosPorRegionCsvUrl: process.env.REACT_APP_FALLECIDOS_POR_REGION_CSV_URL,
-  confirmadosPorRegionCsvUrl: process.env.REACT_APP_CONFIRMADOS_POR_REGION_CSV_URL,
-
-  backupActivosPorComunaCsvUrl: process.env.REACT_APP_BACKUP_ACTIVOS_POR_COMUNA_CSV_URL,
-  backupTotalesNacionalesCsvUrl: process.env.REACT_APP_BACKUP_TOTALES_NACIONALES_CSV_URL,
-  backupFallecidosPorRegionCsvUrl: process.env.REACT_APP_BACKUP_FALLECIDOS_POR_REGION_CSV_URL,
-  backupConfirmadosPorRegionCsvUrl: process.env.REACT_APP_BACKUP_CONFIRMADOS_POR_REGION_CSV_URL,
-
   chileDataJsonUrl: process.env.REACT_APP_CHILE_DATA_JSON_URL,
 };
