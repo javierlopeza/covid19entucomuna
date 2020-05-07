5c147f8424384642a6198c804ea7bb34644891a0
Javier López <javierlopez@uc.cl>
2020-05-07 04:21:16 -0400

    refactor(formatter): name exports

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,10 +1,5 @@
 import moment from './date';
 
-const valueFormatter = value => value.toLocaleString('de-DE');
+export const valueFormatter = value => value.toLocaleString('de-DE');
 
-const dateFormatter = dateStr => moment(dateStr).format('DD-MMMM');
-
-export default {
-  valueFormatter,
-  dateFormatter,
-};
+export const dateFormatter = dateStr => moment(dateStr).format('DD-MMMM');
