3fdc7391d74ffa0a9aff031768cae32fa5ac998f
Javier López <javierlopez@uc.cl>
2020-05-19 15:23:08 -0400

    feat(sentry): add environment

--- src/environment.js
+++ src/environment.js
@@ -1,4 +1,5 @@
 export default {
+  env: process.env.REACT_APP_ENV,
   chileDataJsonUrl: process.env.REACT_APP_CHILE_DATA_JSON_URL,
   currentNotificationJsonUrl: process.env.REACT_APP_CURRENT_NOTIFICATION_JSON_URL,
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/browser';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import environment from './environment';
 
-Sentry.init({dsn: "https://9ce7093c36e847fd944213e2448bf2ec@o394949.ingest.sentry.io/5246008"});
+Sentry.init({
+    dsn: "https://9ce7093c36e847fd944213e2448bf2ec@o394949.ingest.sentry.io/5246008",
+    environment: environment.env,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
