f1e6828a872f50ee191db7e2313496ca04b253e2
Javier López <javierlopez@uc.cl>
2020-05-24 01:37:26 -0400

    feat(faq): add text animation

--- src/components/FAQButton.jsx
+++ src/components/FAQButton.jsx
@@ -1,28 +1,77 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { Component } from 'react';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
-const FAQButton = () => (
-  <Container to="preguntas-frecuentes">
-    <Icon icon={faQuestionCircle} />
-  </Container>
-);
+class FAQButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ showText: false });
+    }, 3000);
+  }
+
+  render() {
+    const { showText } = this.state;
+    return (
+      <Container to="preguntas-frecuentes">
+        <Text show={showText}>Preguntas Frecuentes</Text>
+        <Icon icon={faQuestionCircle} />
+      </Container>
+    );
+  }
+}
 
 export default FAQButton;
 
 const Container = styled(Link)`
   display: flex;
+  flex-direction: row;
+  align-items: center;
   margin: 0.5em;
   position: absolute;
   right: 0;
   bottom: 0;
 `;
 
+const Text = styled.div`
+  z-index: 1;
+  font-size: 0.75em;
+  font-weight: 400;
+  color: ${({ theme }) => theme.colors.red.normal};
+  overflow: hidden;
+
+  background-color: white;
+  border-radius: 0.75em;
+  padding: 0.25em 0.5em;
+  width: 11em;
+  white-space: nowrap;
+
+  ${({ show }) => !show
+    && css`
+      opacity: 0;
+      width: 0.5em;
+      transition: opacity 25ms linear 325ms, width 400ms ease-in-out;
+    `}
+
+  :hover {
+    opacity: 1;
+    width: 11em;
+    transition: opacity 25ms linear, width 300ms ease-out;
+  }
+`;
+
 const Icon = styled(FontAwesomeIcon)`
+  position: absolute;
+  right: 0;
+
   cursor: pointer;
-  font-size: 0.8em;
+  font-size: 1.1em;
   color: white;
 
   :hover + div {
