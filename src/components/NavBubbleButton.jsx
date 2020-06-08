b51d8ba2807cf9823899e752ed920883cfe5040d
Javier López <javierlopez@uc.cl>
2020-06-08 17:28:43 -0400

    feat(rankings): add nav bubble button

--- src/components/NavBubbleButton.jsx
+++ src/components/NavBubbleButton.jsx
@@ -1,13 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 const NavBubbleButton = (props) => {
   const { children, path, onClick: clickHandler } = props;
   return (
-    <NavLink exact to={path} onClick={clickHandler}>
+    <Container exact to={path} onClick={clickHandler}>
       {children}
-    </NavLink>
+    </Container>
   );
 };
 
 export default NavBubbleButton;
+
+const Container = styled(NavLink)`
+  padding: 0.5em 0.75em;
+  border-radius: 8px;
+  background-color: white;
+  ${({ theme }) => theme.baseShadow}
+  color: ${({ theme }) => theme.colors.gray.normal};
+  font-weight: 200;
+
+  :active, :visited {
+    color: ${({ theme }) => theme.colors.gray.normal};
+  }
+
+  &.active {
+    background-color: ${({ theme }) => theme.colors.blue.normal};
+    color: white;
+    font-weight: 400;
+  }
+
+  :not(.active) {
+    transition: all 0.3s ease-in-out;
+
+    @media (hover: hover) {
+      background: linear-gradient(
+        to left,
+        white 50%,
+        ${({ theme }) => theme.colors.blue.normal} 50%
+      );
+      background-size: 201% 100%;
+      background-position: right bottom;
+
+      :hover {
+        color: white;
+        background-position: left bottom;
+        transition: all 0.3s ease-in-out;
+      }
+    }
+  }
+`;
